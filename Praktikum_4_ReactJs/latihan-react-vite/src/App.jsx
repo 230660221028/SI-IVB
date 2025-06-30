be2382d2825ed086de4a01e050fff0644c549bec
230660221028 <230660221028@student.unsap.ac.id>
2025-07-01 00:20:57 +0700

    Menambahkan Praktikum ReactJs dan Menambahkan Praktikum 3-JS

---
+++ Praktikum_4_ReactJs/latihan-react-vite/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <h1 className="title">Welcome!</h1>
+    <p className="text">Nama saya Fitri Cahyani saya mahasisawi dari Universitas Sebelas April, Fakultas Teknologi Informasi, Prodi Sistem Informasi, kelas SI-IV B. Saya lahir di Sumedang pada tanggal 15 Desember 2003</p>
+   </div> 
+  )
+}
+
+export default App
